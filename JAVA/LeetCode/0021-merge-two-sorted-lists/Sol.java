07a8a607891089aed67afae2aada344851a0b1bd
Parth sudani <parths6028@gmail.com>
2026-08-22 16:53:17 +0530

    Add solution for 0021-merge-two-sorted-lists

---
+++ JAVA/LeetCode/0021-merge-two-sorted-lists/Sol.java
@@ -0,0 +1,13 @@
+
+        if (temp1 != null) {
+            temp.next = temp1;
+        }
+
+        if (temp2 != null) {
+            temp.next = temp2;
+        }
+
+        return dummy.next;
+    }
+}
+        }
