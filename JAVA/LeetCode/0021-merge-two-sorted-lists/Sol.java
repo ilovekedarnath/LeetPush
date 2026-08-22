7e7571bf62dd19e2fb8274b0b40ed4c8b8a48fd7
Parth sudani <parths6028@gmail.com>
2026-08-22 16:55:18 +0530

    Update solution for 0021-merge-two-sorted-lists

--- JAVA/LeetCode/0021-merge-two-sorted-lists/Sol.java
+++ JAVA/LeetCode/0021-merge-two-sorted-lists/Sol.java
@@ -1,13 +1,12 @@
-
-        if (temp1 != null) {
-            temp.next = temp1;
-        }
-
-        if (temp2 != null) {
-            temp.next = temp2;
-        }
-
-        return dummy.next;
+ (nullptr) {}
+ *     ListNode(int x, ListNode *next) : val
+ (x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, 
+    ListNode* list2) {
+        
     }
-}
-        }
+};
